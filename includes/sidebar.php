e72ed528d5ac5158b091892c93fd3d246e3d9013
FatinFityah <126367282+FatinFityah@users.noreply.github.com>
2026-07-21 10:03:23 +0800

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,98 @@
+<div class="sidebar">
+
+<div class="logo">
+
+<i class="fa-solid fa-car-side"></i>
+
+<h4>TEGAS</h4>
+
+<p>Vehicle Management</p>
+
+</div>
+
+<ul>
+
+<li>
+
+<a href="../superadmin/dashboard.php">
+
+<i class="fa-solid fa-house"></i>
+
+Dashboard
+
+</a>
+
+</li>
+
+<li>
+
+<a href="../superadmin/vehicles/index.php">
+    <i class="fa-solid fa-car"></i>
+    Vehicles
+</a>
+
+</a>
+
+</li>
+
+<li>
+
+<a href="#">
+
+<i class="fa-solid fa-calendar-check"></i>
+
+Bookings
+
+<a href="/tegas-vms/superadmin/bookings/index.php">
+
+<i class="fa-solid fa-calendar-check"></i>
+
+Bookings
+
+</a>
+
+Staff
+
+</a>
+
+</li>
+
+<li>
+
+<a href="#">
+
+<i class="fa-solid fa-building"></i>
+
+Departments
+
+</a>
+
+</li>
+
+<li>
+
+<a href="/tegas-vms-v2/admin/reports.php">
+
+<i class="fa-solid fa-chart-column"></i>
+
+Reports
+
+</a>
+
+</li>
+
+<li>
+
+<a href="/tegas-vms-v2/logout.php">
+
+<i class="fa-solid fa-right-from-bracket"></i>
+
+Logout
+
+</a>
+
+</li>
+
+</ul>
+
+</div>
