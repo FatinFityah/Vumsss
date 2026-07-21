93d4869cd1ce0034afa2eb945982a8e98e2521f8
FatinFityah <126367282+FatinFityah@users.noreply.github.com>
2026-07-21 09:20:07 +0800

    Create quick_actions.php

---
+++ admin/includes/quick_actions.php
@@ -0,0 +1,43 @@
+<div class="mt-5">
+
+    <h3 class="page-title mb-4">
+        ⚡ Quick Actions
+    </h3>
+
+    <div class="row g-4">
+
+        <div class="col-md-3">
+            <a href="approvals.php" class="action-card">
+                <i class="fa-solid fa-circle-check"></i>
+                <h5>Approval Centre</h5>
+                <p>Approve or reject requests</p>
+            </a>
+        </div>
+
+        <div class="col-md-3">
+            <a href="vehicles/index.php" class="action-card">
+                <i class="fa-solid fa-car"></i>
+                <h5>Vehicle Management</h5>
+                <p>Manage vehicles</p>
+            </a>
+        </div>
+
+        <div class="col-md-3">
+            <a href="maintenance.php" class="action-card">
+                <i class="fa-solid fa-screwdriver-wrench"></i>
+                <h5>Maintenance</h5>
+                <p>Vehicle servicing</p>
+            </a>
+        </div>
+
+        <div class="col-md-3">
+            <a href="reports.php" class="action-card">
+                <i class="fa-solid fa-chart-column"></i>
+                <h5>Reports</h5>
+                <p>View reports</p>
+            </a>
+        </div>
+
+    </div>
+
+</div>
