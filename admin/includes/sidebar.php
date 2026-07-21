afa5fa6229ac652426d57f9101095f26e2e73e70
FatinFityah <126367282+FatinFityah@users.noreply.github.com>
2026-07-21 09:20:28 +0800

    Create sidebar.php

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,51 @@
+<div class="col-lg-2 sidebar">
+
+    <div class="logo text-center mt-4">
+
+        <i class="fa-solid fa-user-shield fa-3x"></i>
+
+        <h3 class="mt-3">TEGAS Fleet</h3>
+
+        <p>Fleet Management</p>
+
+    </div>
+
+    <hr>
+
+    <a href="/tegas-vms-v2/admin/dashboard.php">
+        <i class="fa-solid fa-house"></i> Dashboard
+    </a>
+
+    <a href="/tegas-vms-v2/admin/approvals.php">
+        <i class="fa-solid fa-circle-check"></i> Approval Centre
+    </a>
+
+    
+
+    <a href="/tegas-vms-v2/admin/vehicles/index.php">
+        <i class="fa-solid fa-car"></i> Vehicle Management
+    </a>
+
+  
+
+    <a href="/tegas-vms-v2/admin/return_verification.php">
+        <i class="fa-solid fa-clipboard-check"></i> Return Verification
+    </a>
+
+    <a href="/tegas-vms-v2/admin/return_history.php">
+        <i class="fa-solid fa-clock-rotate-left"></i> Return History
+    </a>
+
+    <a href="/tegas-vms-v2/admin/reports.php">
+        <i class="fa-solid fa-chart-column"></i> Reports
+    </a>
+
+    <a href="/tegas-vms-v2/admin/profile.php">
+        <i class="fa-solid fa-user"></i> My Profile
+    </a>
+
+    <a href="/tegas-vms-v2/logout.php">
+        <i class="fa-solid fa-right-from-bracket"></i> Logout
+    </a>
+
+</div>
